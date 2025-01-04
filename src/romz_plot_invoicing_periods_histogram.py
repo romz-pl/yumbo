83ee90e4dcbc6010ee0ae860c41e4b3f6e03533b
romz <romz@wp.pl>
2025-01-04 15:54:04 +0100

    Optimize function romz_plot_invoicing_periods_histogram.plot

--- src/romz_plot_invoicing_periods_histogram.py
+++ src/romz_plot_invoicing_periods_histogram.py
@@ -9,30 +9,39 @@ def plot(invper, schedule, bounds, dpi):
         st.write(":green[No limits have been set for the invoicing periods.]")
         return
 
-    y = pd.Series(range(len(bounds.index)))
+    # Calculate workload for each period
+    y = []
     for k in bounds.index:
         period_name = bounds.at[k, "Period"]
-        start = invper[invper["Name"] == period_name]["Start day"].iat[0]
-        end = invper[invper["Name"] == period_name]["End day"].iat[0]
+        period_data = invper[invper["Name"] == period_name]
+        start, end = period_data["Start day"].iat[0], period_data["End day"].iat[0]
         x_task = pd.date_range(start=start, end=end, freq="D").astype("str").intersection(schedule.columns)
-        y[k] = schedule.loc[:, x_task].sum().sum()
+        y.append(schedule.loc[:, x_task].sum().sum())
 
+    y = pd.Series(y)
     ylower = bounds["Lower"].to_numpy()
     yupper = bounds["Upper"].to_numpy()
-    yerr = [[0],[0]]
-    yerr[0] = y - ylower
-    yerr[1] = yupper - y
+    yerr = [y - ylower, yupper - y]
 
+    # Create the plot
     fig = Figure(figsize=(8, 4))
     ax = fig.subplots()
-    ax.set_ylabel('Hours')
-    ax.set_title('Invoicing periods workload')
-    ax.bar(bounds["Period"], y, yerr=yerr, color='#7BC8F6', ecolor='tab:red', capsize=4)
-    ax.tick_params(axis='x', rotation=0, labelsize='x-small')
-    ax.tick_params(axis='y', labelsize='x-small')
+    ax.set_ylabel("Hours")
+    ax.set_title("Invoicing Periods Workload")
+    ax.bar(
+        bounds["Period"],
+        y,
+        yerr=yerr,
+        color="#7BC8F6",
+        ecolor="tab:red",
+        capsize=4,
+    )
+    ax.tick_params(axis="x", rotation=0, labelsize="x-small")
+    ax.tick_params(axis="y", labelsize="x-small")
 
+    # Finalize and display the plot
     fig.tight_layout()
-    buf = io.BytesIO()
-    fig.savefig(buf, format='png', dpi=dpi, pil_kwargs={'compress_level': 1})
-    st.image(buf)
-    buf.close()
+    with io.BytesIO() as buf:
+        fig.savefig(buf, format="png", dpi=dpi, pil_kwargs={"compress_level": 1})
+        buf.seek(0)
+        st.image(buf)
