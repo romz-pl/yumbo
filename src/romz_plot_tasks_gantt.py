bea1c87064c66fb4daf655879316aa83b8c5f22d
romz <romz@wp.pl>
2025-01-04 10:58:27 +0100

    Change date orientation and appearance on summary Tasks Gannt Chart

--- src/romz_plot_tasks_gantt.py
+++ src/romz_plot_tasks_gantt.py
@@ -12,12 +12,13 @@ def plot_summary(df, dpi):
 
     rects = ax.barh(y=df["Name"], width=df["Days"]-1, left=df["Start day"], color='tab:orange', height=0.9, alpha=0.6)
 
-    ax.bar_label(rects, labels=df["Name"], size=6, label_type='center')
+    # ax.bar_label(rects, labels=df["Name"], size=6, label_type='center')
 
-    ax.yaxis.set_major_locator(tck.NullLocator())
+    ax.yaxis.set_major_locator(tck.MaxNLocator(5, integer=True))
     ax.set_xticks(df["End day"])
-    ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
-    ax.tick_params(axis='x', rotation=0, labelsize='x-small')
+    # ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
+    ax.tick_params(axis='x', rotation=90, labelsize='x-small')
+    ax.tick_params(axis='y', rotation=0, labelsize='x-small')
     ax.yaxis.grid()
     ax.set_axisbelow(True)
     ax.set_ylim(bottom=-0.6)
