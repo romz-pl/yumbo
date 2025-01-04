00d7e8e2caa1b460dcec853d509426eec3f1a652
romz <romz@wp.pl>
2025-01-04 15:20:56 +0100

    Optimize function romz_plot_tasks_gantt.plot_summary

--- src/romz_plot_tasks_gantt.py
+++ src/romz_plot_tasks_gantt.py
@@ -8,29 +8,42 @@ import streamlit as st
 
 
 def plot_summary(df, dpi, today):
+    # Create figure and axis
     fig = Figure(figsize=(8, 4))
     ax = fig.subplots()
-    ax.set_title("Task's Gantt chart")
+    ax.set_title("Task's Gantt Chart")
 
-    rects = ax.barh(y=df["Name"], width=df["Days"]-1, left=df["Start day"], color='tab:orange', height=0.9, alpha=0.6)
+    # Plot Gantt bars
+    ax.barh(
+        y=df["Name"],
+        width=df["Days"] - 1,
+        left=df["Start day"],
+        color="tab:orange",
+        height=0.9,
+        alpha=0.6,
+    )
 
-    # ax.bar_label(rects, labels=df["Name"], size=6, label_type='center')
-
+    # Configure x-axis
     ax.xaxis.set_major_locator(tck.MaxNLocator(5, integer=True))
-    ax.yaxis.set_major_locator(tck.MaxNLocator(5, integer=True))
-    # ax.set_xticks(df["End day"])
-    # ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
-    ax.tick_params(axis='x', rotation=0, labelsize='x-small')
-    ax.tick_params(axis='y', rotation=0, labelsize='x-small')
-    ax.yaxis.grid()
-    ax.set_axisbelow(True)
+    ax.tick_params(axis="x", rotation=0, labelsize="x-small")
     ax.set_xlim(left=np.datetime64(today))
+
+    # Configure y-axis
+    ax.yaxis.set_major_locator(tck.MaxNLocator(5, integer=True))
+    ax.tick_params(axis="y", rotation=0, labelsize="x-small")
+    ax.yaxis.grid(alpha=0.5)
+    ax.set_axisbelow(True)
+
+    # Configure layout
     ax.set_ylim(bottom=-0.6)
     fig.tight_layout()
-    buf = io.BytesIO()
-    fig.savefig(buf, format='png', dpi=dpi, pil_kwargs={'compress_level': 1})
-    st.image(buf)
-    buf.close()
+
+    # Save the figure to a buffer and display
+    with io.BytesIO() as buf:
+        fig.savefig(buf, format="png", dpi=dpi, pil_kwargs={"compress_level": 1})
+        buf.seek(0)
+        st.image(buf)
+
 
 def plot(df, work_done, dpi, today):
     # Create figure and axis
