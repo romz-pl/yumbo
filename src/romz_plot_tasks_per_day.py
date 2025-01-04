fc6ef10e22edb61299828f263658b2798b642f5d
romz <romz@wp.pl>
2025-01-04 16:05:04 +0100

    Optimize function romz_plot_tasks_per_day.plot

--- src/romz_plot_tasks_per_day.py
+++ src/romz_plot_tasks_per_day.py
@@ -2,9 +2,14 @@ import pandas as pd
 import romz_plot_per_day
 
 def plot(df, start_day, end_day, dpi):
-    if end_day < start_day:
-        end_day = start_day
+    # Ensure the end day is not earlier than the start day
+    end_day = max(end_day, start_day)
 
-    days = pd.period_range(start=start_day, end=end_day, freq="D").astype("str")
+    # Generate date range as strings
+    days = pd.period_range(start=start_day, end=end_day, freq="D").astype(str)
+
+    # Count the number of tasks per day
     tasks_per_day = (df[days] > 0).sum()
-    romz_plot_per_day.plot(df, days, tasks_per_day, 'Tasks per day', 'C3', '\\', dpi)
+
+    # Use the plotting function to visualize the data
+    romz_plot_per_day.plot(df, days, tasks_per_day, "Tasks per day", "C3", "\\", dpi)
