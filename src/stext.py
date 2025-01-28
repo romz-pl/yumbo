27b53148f1c29e72bd34dd1095aea1675d8fa49b
romz <romz@wp.pl>
2025-01-28 11:21:05 +0100

    Add comment depicting how to count number of bytes in buffer

--- src/stext.py
+++ src/stext.py
@@ -9,5 +9,6 @@ def show_fig(fig):
     with io.BytesIO() as buf:
         fig.savefig(buf, format="WebP", pil_kwargs={"lossless":True, "quality":70, "method":3} )
         # fig.savefig(buf, format="png", pil_kwargs={"compress_level": 9})
+        # st.write(buf.getbuffer().nbytes)
         st.image(buf)
 
