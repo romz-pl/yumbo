08098850df7847fce45d828406c6ea1fd0de048b
romz <romz@wp.pl>
2025-01-31 15:52:13 +0100

    Add XBDAY constrains to sparse AMPL model

--- sparse_model/ampl.mod.py
+++ sparse_model/ampl.mod.py
@@ -1,21 +1,16 @@
+
+# The maximal total number of working hours per day
+param HOURS_PER_DAY > 0 integer;
+
 # Set of task names to be included in the schedule
-set TASKN ordered;
+set TASKN;
 
+# The scope of each task
+set SCOPE{TASKN} ordered;
 
-set TASKD{TASKN};
+# The work of a task:
+param WORK{TASKN} integer, >= 0;
 
-
-
-# The set of tasks definition:
-param TASKS{TASKN} >= 0 integer; # START
-param TASKE{TASKN} >= 0 integer; # END
-param TASKW{TASKN} >= 0 integer; # WORK
-
-check {t in TASKN}:
-    0 <= TASKS[t] <= TASKE[t];
-
-check {t in TASKN}:
-    0 <= TASKW[t];
 
 
 # Set of expert names to be included in the schedule
@@ -23,13 +18,32 @@ set EXPERTN ordered;
 
 set LINK within {EXPERTN, TASKN};
 
+set XBDAY within {(e, t) in LINK, SCOPE[t]};
 
-param BOUND{(e, t) in LINK, TASKD[t]} >= 0 integer;
-
-param BBWW{t in TASKN, TASKD[t]} >= 0 integer;
-
-# Objective function. This function has the clear meaning, fortunatelly.
-#minimize objective_function:
-#    sum {e in EXPERTN, t in TASKN} TASKDEV[e, t];
+# The set of bounds definition: LOWER, UPPER
+param XBL{XBDAY};
+param XBU{XBDAY};
 
 
+# X[e, t, d] means the number of hours assigned to expert "e" for task "t" on day "d" 
+var X{(e, t) in LINK, SCOPE[t]} integer, >= 0, <= HOURS_PER_DAY;
+
+# Objective function.
+# This function encourages the early completion of tasks.
+minimize objective_function:
+    sum {(e, t) in LINK, d in SCOPE[t]} ((d + 1 - first(SCOPE[t]))^(1/3)) * X[e, t, d];
+
+
+# The total number of working hours per day
+#subject to C_hours_per_day {e in EXPERTN, d in SCOPE[t]}:
+#    sum {(e, t) in LINKS} X[e, t, d] <= HOURS_PER_DAY;
+
+# The total number of working hours in the task
+subject to C_work {t in TASKN}:
+    sum {(e, t) in LINK, d in SCOPE[t]} X[e, t, d] = WORK[t];
+
+# Constraines for number of hours
+subject to C_xbday {(e, t, d) in XBDAY}:
+    XBL[e, t, d] <= X[e, t, d ] <= XBU[e, t, d];
+
+ 
