a129e17078ebc2570627f410102da50c9ce7be30
romz <romz@wp.pl>
2025-01-31 11:15:57 +0100

    Create the first version of sparse AMPL model

---
+++ sparse_model/ampl.mod.py
@@ -0,0 +1,35 @@
+# Set of task names to be included in the schedule
+set TASKN ordered;
+
+
+set TASKD{TASKN};
+
+
+
+# The set of tasks definition:
+param TASKS{TASKN} >= 0 integer; # START
+param TASKE{TASKN} >= 0 integer; # END
+param TASKW{TASKN} >= 0 integer; # WORK
+
+check {t in TASKN}:
+    0 <= TASKS[t] <= TASKE[t];
+
+check {t in TASKN}:
+    0 <= TASKW[t];
+
+
+# Set of expert names to be included in the schedule
+set EXPERTN ordered;
+
+set LINKS within {EXPERTN, TASKN};
+
+
+# param BOUND{(e, t) in LINKS, TASKD[t]} >= 0 integer;
+
+param BOUND{t in TASKN, TASKD[t]} >= 0 integer;
+
+# Objective function. This function has the clear meaning, fortunatelly.
+#minimize objective_function:
+#    sum {e in EXPERTN, t in TASKN} TASKDEV[e, t];
+
+
