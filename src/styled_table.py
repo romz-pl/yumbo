53d5e8d617f7acba07a8d5a2437c4b4b97dbe8e3
romz <romz@wp.pl>
2025-02-10 20:54:12 +0100

    Split and refactor function [styled_table.show]

--- src/styled_table.py
+++ src/styled_table.py
@@ -18,6 +18,62 @@ def format_cell(value):
     return value  # Return non-float values unchanged
 
 
+def show_stable(df, styles):
+    #
+    # The "format_index" does not work with st.dataframe in Streamlit version 1.42.0!
+    # So, a temporray column "Date" has been added and the index has been hidden.
+    #
+
+    # Create temporary column for Weekday and Date
+    df.insert(0, "Weekday", df.index.strftime('%a'))
+    df.insert(0, "Date", df.index.strftime("%Y-%m-%d"))
+
+    styled_df = (
+        df.style
+        .format(format_cell)
+        # .format_index("{:%Y-%m-%d}", axis=0) It does not work!
+        .apply(highlight_rows, axis=1)
+        .set_table_styles(styles, overwrite=True)
+        .map(lambda _: 'color:LightBlue', subset=['Date', 'Weekday'])
+    )
+
+    # Render the styled DataFrame as Streamlit dataframe
+    st.dataframe(styled_df, hide_index=True)
+
+    csv = glb.convert_df_to_csv(df)
+
+    st.download_button(
+        label=f"Download schedule for :green[{df.name}] as CSV",
+        data=csv,
+        file_name=f"{st.session_state.mprob['uploaded_file_name']}_{df.name}_full_schedule.csv",
+        mime="text/csv",
+    )
+
+    # Drop the temporary column for Weekday and Date
+    df.drop(columns="Date", inplace=True)
+    df.drop(columns="Weekday", inplace=True)
+
+
+def show_htable(df, styles):
+    # Temporary column for Weekday
+    df.insert(0, "Weekday", df.index.strftime('%a'))
+
+    styled_df = (
+        df.style
+        .format(format_cell)
+        .format_index("{:%Y-%m-%d}", axis=0)
+        .apply(highlight_rows, axis=1)
+        .set_table_styles(styles, overwrite=True)
+        .map(lambda _: 'color:LightBlue', subset=['Weekday'])
+    )
+
+    # Render the styled DataFrame as HTML
+    st.markdown(styled_df.to_html(), unsafe_allow_html=True)
+
+    # Drop the temporary Weekday column
+    df.drop(columns="Weekday", inplace=True)
+
+
 def show(df, as_html):
     # Define table styles as a list for clarity
     styles = [
@@ -26,51 +82,7 @@ def show(df, as_html):
         {'selector': 'th:not(.index_name)', 'props': 'background-color: #000066; color: white;'}
     ]
 
-    # Temporary column for Weekday
-    df.insert(0, "Weekday", df.index.strftime('%a'))
-
     if as_html:
-        # Render the styled DataFrame as HTML
-        styled_df = (
-            df.style
-            .format(format_cell)
-            .format_index("{:%Y-%m-%d}", axis=0)
-            .apply(highlight_rows, axis=1)
-            .set_table_styles(styles, overwrite=True)
-            .map(lambda _: 'color:LightBlue', subset=['Weekday'])
-        )
-        st.markdown(styled_df.to_html(), unsafe_allow_html=True)
+        show_htable(df, styles)
     else:
-        #
-        # The "format_index" does not work with st.dataframe in Streamlit version 1.42.0!
-        # So, a temporray column "Date" has been added and the index has been hidden.
-        #
-
-        # Temporary column for Date.
-        df.insert(0, "Date", df.index.strftime("%Y-%m-%d"))
-
-        styled_df = (
-            df.style
-            .format(format_cell)
-            # .format_index("{:%Y-%m-%d}", axis=0) It does not work!
-            .apply(highlight_rows, axis=1)
-            .set_table_styles(styles, overwrite=True)
-            .map(lambda _: 'color:LightBlue', subset=['Date', 'Weekday'])
-        )
-        st.dataframe(styled_df, hide_index=True)
-
-        csv = glb.convert_df_to_csv(df)
-
-        st.download_button(
-            label=f"Download schedule for :green[{df.name}] as CSV",
-            data=csv,
-            file_name=f"{st.session_state.mprob['uploaded_file_name']}_{df.name}_full_schedule.csv",
-            mime="text/csv",
-        )
-
-        # Drop the temporary Date column
-        df.drop(columns="Date", inplace=True)
-
-    # Drop the temporary Weekday column
-    df.drop(columns="Weekday", inplace=True)
-
+        show_stable(df, styles)
