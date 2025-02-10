16d9b923d33441888e063ec14677ca135d85026f
romz <romz@wp.pl>
2025-02-10 19:28:37 +0100

    Extend and improve function [styled_table.show]

--- src/styled_table.py
+++ src/styled_table.py
@@ -20,11 +20,8 @@ def format_score(value):
         return f"{value:.2f}"  # Round to 1 decimal place
     return value  # Return non-float values unchanged
 
+
 def show(df, as_html):
-    df.insert(0, "Weekday", df.index.strftime('%a'))
-
-    # df.replace(0, np.nan, inplace=True)
-
     # Define table styles as a list for clarity
     styles = [
         {'selector': 'tr:hover', 'props': [('background-color', '#555555')]},
@@ -32,36 +29,42 @@ def show(df, as_html):
         {'selector': 'th:not(.index_name)', 'props': 'background-color: #000066; color: white;'}
     ]
 
-    # # Apply styling to the DataFrame using method chaining with parentheses for better readability
-    # styled_df = (
-    #     df.style
-    #     .format_index("{:%Y-%m-%d}", axis=0)
-    #     #.format(lambda x: '{:.2f}'.format(x) if isinstance(x, (int, float)) and x > 0 else '') # Represent NaN as empty string!
-    #     .format(func),
-    #     # .apply(highlight_rows, axis=1)
-    #     #.set_table_styles(styles, overwrite=True)
-    #     #.map(lambda v: 'color:LightBlue', subset=['Weekday'])
-    # )
-
-
-
-    # Apply the custom function to each cell in the DataFrame
-    styled_df = (
-        df.style
-        .format(format_score)
-        .format_index("{:%Y-%m-%d}", axis=0)
-        .apply(highlight_rows, axis=1)
-        .set_table_styles(styles, overwrite=True)
-        .map(lambda v: 'color:LightBlue', subset=['Weekday'])
-    )
-
+    # Temporary column for Weekday
+    df.insert(0, "Weekday", df.index.strftime('%a'))
 
     if as_html:
-        # Render the styled dataframe as HTML.
+        # Render the styled DataFrame as HTML
+        styled_df = (
+            df.style
+            .format(format_score)
+            .format_index("{:%Y-%m-%d}", axis=0)
+            .apply(highlight_rows, axis=1)
+            .set_table_styles(styles, overwrite=True)
+            .map(lambda _: 'color:LightBlue', subset=['Weekday'])
+        )
         st.markdown(styled_df.to_html(), unsafe_allow_html=True)
     else:
-        # Display the dataframe as a Streamlit table.
-        # Some of the formats may not be displayed correctly!
-        st.dataframe(styled_df, use_container_width=False)
+        #
+        # The "format_index" does not work with st.dataframe in Streamlit version 1.42.0!
+        # So, a temporray column "Date" has been added and the index has been hidden.
+        #
 
+        # Temporary column for Date
+        df.insert(0, "Date", df.index.strftime("%Y-%m-%d"))
+
+        styled_df = (
+            df.style
+            .format(format_score)
+            # .format_index("{:%Y-%m-%d}", axis=0) It does not work!
+            .apply(highlight_rows, axis=1)
+            .set_table_styles(styles, overwrite=True)
+            .map(lambda _: 'color:LightBlue', subset=['Date', 'Weekday'])
+        )
+        st.dataframe(styled_df, hide_index=True)
+
+        # Drop the temporary Date column
+        df.drop(columns="Date", inplace=True)
+
+    # Drop the temporary Weekday column
     df.drop(columns="Weekday", inplace=True)
+
